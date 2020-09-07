be5ffa4bdb6095d58104a6b65f6afb30074938b1
fedef17 <frithurik88@gmail.com>
2020-09-07 17:56:55 +0200

    low trans region

---
+++ trans_region_low.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sys
+import os
+
+from matplotlib import pyplot as plt
+from matplotlib import cm
+#import climtools_lib as ctl
+
+from scipy import io
+import scipy.constants as const
+import pickle
+from scipy.interpolate import PchipInterpolator as spline
+
+if os.uname()[1] == 'ff-clevo':
+    sys.path.insert(0, '/home/fedefab/Scrivania/Research/Post-doc/git/SpectRobot/')
+    sys.path.insert(0, '/home/fedefab/Scrivania/Research/Post-doc/git/pythall/')
+    cart_out = '/home/fedefab/Scrivania/Research/Post-doc/CO2_cooling/new_param/LTE/'
+else:
+    sys.path.insert(0, '/home/fabiano/Research/git/SpectRobot/')
+    sys.path.insert(0, '/home/fabiano/Research/git/pythall/')
+    cart_out = '/home/fabiano/Research/lavori/CO2_cooling/new_param/LTE/'
+
+import newparam_lib as npl
+
+##############################################################
+kbc = const.k/(const.h*100*const.c) # 0.69503
+kboltz = 1.38064853e-23 # J/K
+E_fun = 667.3799 # cm-1 energy of the 0110 -> 0000 transition
+
+cp = 1.005e7 # specific enthalpy dry air - erg g-1 K-1
+#############################################################
+
+allatms = ['mle', 'mls', 'mlw', 'tro', 'sas', 'saw']
+atmweigths = [0.3, 0.1, 0.1, 0.4, 0.05, 0.05]
+atmweigths = dict(zip(allatms, atmweigths))
+allco2 = np.arange(1,8)
+
+all_coeffs = pickle.load(open(cart_out + 'all_coeffs_LTE_v2.p'))
+atm_pt = pickle.load(open(cart_out + 'atm_pt_v2.p'))
+
+n_alts = 54
+
+all_alts = atm_pt[('mle', 'alts')]
+alts = atm_pt[('mle', 'alts')][:n_alts]
+
+n_alts_lte = 40
+
+tot_coeff_co2 = pickle.load(open(cart_out + 'tot_coeffs_co2_v2_LTE.p', 'rb'))
+
+# Now. We are in the low transition region and need to adjust the LTE coeffs to non-LTE.
+nlte_coeffs = dict()
