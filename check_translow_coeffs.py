5c56f11e7c551331002bb90c869d14ac67241a4f
fedef17 <frithurik88@gmail.com>
2020-09-13 16:56:28 +0200

    low trans region check

--- check_translow_coeffs.py
+++ check_translow_coeffs.py
@@ -147,7 +147,7 @@ for ii, atm in enumerate(allatms):
 
     ax.set_title(atm)
     ax.set_ylim(20, 90)
-    ax.xscale('log')
+    ax.set_xscale('log')
     ax.set_xlim(0.1, 1000)
     ax.grid()
 
@@ -155,7 +155,7 @@ plt.legend()
 fig.savefig(cart_out_2 + 'check_ratios_NLTE_lotr.pdf')
 
 for ax in axes:
-    ax.xscale('lin')
+    ax.set_xscale('linear')
     ax.set_xlim(0, 20)
 fig.savefig(cart_out_2 + 'check_ratios_NLTE_lotr_zoom.pdf')
 
