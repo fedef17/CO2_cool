f64d48ee32f4dcbbd409332463cc4d2c71544ece
Federico Fabiano <f.fabiano@isac.cnr.it>
2022-05-26 11:59:20 +0200

    mipas fix co2 check

--- MIPAS_cool_new.py
+++ MIPAS_cool_new.py
@@ -105,191 +105,190 @@ co2column_debug = []
 
 debug_alphafit = []
 
-inputs = dict()
-for nam in ['temp', 'pres', 'ovmr', 'co2vmr', 'o2vmr', 'n2vmr', 'cr_mipas', 'x']:
-    inputs[nam] = []
+do_calc = False
+if do_calc:
+    inputs = dict()
+    for nam in ['temp', 'pres', 'ovmr', 'co2vmr', 'o2vmr', 'n2vmr', 'cr_mipas', 'x']:
+        inputs[nam] = []
 
-mipx = []
-for il in range(len(CR)):
-    print(il)
-    temp_or = T.target[il]
+    mipx = []
+    for il in range(len(CR)):
+        print(il)
+        temp_or = T.target[il]
 
-    #T.target_ig[il]
-    pres_or = T.pressure[il]
-    #P.target_ig[il]
-    x_or = np.log(1000./pres_or) # SHOULD USE THIS FOR INTERPOLATING INSTEAD?
+        #T.target_ig[il]
+        pres_or = T.pressure[il]
+        #P.target_ig[il]
+        x_or = np.log(1000./pres_or) # SHOULD USE THIS FOR INTERPOLATING INSTEAD?
 
-    Ocon_or = O.target[il]
-    splO = spline(O.altitude[il], np.log(Ocon_or), extrapolate = False)
-    Ocon = splO(alts)
-    Ocon = np.exp(Ocon)
+        Ocon_or = O.target[il]
+        splO = spline(O.altitude[il], np.log(Ocon_or), extrapolate = False)
+        Ocon = splO(alts)
+        Ocon = np.exp(Ocon)
 
-    CO2con_or = CO2.target[il]
-    splCO2 = spline(CO2.altitude[il], CO2con_or, extrapolate = False)
-    CO2con = splCO2(alts)
+        CO2con_or = CO2.target[il]
+        splCO2 = spline(CO2.altitude[il], CO2con_or, extrapolate = False)
+        CO2con = splCO2(alts)
 
-    splT = spline(T.altitude[il],temp_or, extrapolate = False)
-    temp = splT(alts)
+        splT = spline(T.altitude[il],temp_or, extrapolate = False)
+        temp = splT(alts)
 
-    splP = spline(T.altitude[il],np.log(pres_or), extrapolate = False)
-    pres = splP(alts)
-    pres = np.exp(pres)
+        splP = spline(T.altitude[il],np.log(pres_or), extrapolate = False)
+        pres = splP(alts)
+        pres = np.exp(pres)
 
-    x = np.log(1000./pres)
-    mipx.append(x)
+        x = np.log(1000./pres)
+        mipx.append(x)
 
-    o2spl = spline(alt_manuel, mol_vmrs['O2'])
-    O2con = o2spl(alts)
-    n2spl = spline(alt_manuel, mol_vmrs['N2'])
-    N2con = n2spl(alts)
+        o2spl = spline(alt_manuel, mol_vmrs['O2'])
+        O2con = o2spl(alts)
+        n2spl = spline(alt_manuel, mol_vmrs['N2'])
+        N2con = n2spl(alts)
 
-    o2vmr = O2con*1.e-6
-    n2vmr = N2con*1.e-6
-    ovmr = Ocon*1.e-6
-    co2vmr = CO2con*1.e-6
+        o2vmr = O2con*1.e-6
+        n2vmr = N2con*1.e-6
+        ovmr = Ocon*1.e-6
+        co2vmr = CO2con*1.e-6
 
-    inputs['temp'].append(temp)
-    inputs['pres'].append(pres)
-    inputs['ovmr'].append(ovmr)
-    inputs['co2vmr'].append(co2vmr)
-    inputs['o2vmr'].append(o2vmr)
-    inputs['n2vmr'].append(n2vmr)
-    inputs['cr_mipas'].append(CR.target[il])
-    inputs['x'].append(x)
+        inputs['temp'].append(temp)
+        inputs['pres'].append(pres)
+        inputs['ovmr'].append(ovmr)
+        inputs['co2vmr'].append(co2vmr)
+        inputs['o2vmr'].append(o2vmr)
+        inputs['n2vmr'].append(n2vmr)
+        inputs['cr_mipas'].append(CR.target[il])
+        inputs['x'].append(x)
 
-    # filename = cart+'atm_manuel.dat'
-    # sbm.scriviinputmanuel(alt_manuel,temp,pres,filename)
-    #
-    # mol_vmrs['CO2'] = CO2con
-    # mol_vmrs['O'] = Ocon
-    # filename = cart+'vmr_atm_manuel.dat'
-    # sbm.write_input_vmr_man(filename, alt_manuel, mol_vmrs, hit_gas_list = molist, hit_gas_num = molnums, version = 2)
-    #
-    # call(cart+'./fomi_mipas')
-    # nomeout = cart+'output__mipas.dat'
-    # alt_fomi, cr_fomi = sbm.leggioutfomi(nomeout)
-    alt_fomi, x_fomi, cr_fomi = npl.old_param(alts, temp, pres, CO2con, Oprof = Ocon, O2prof = O2con, N2prof = N2con, input_in_ppm = True)
+        # filename = cart+'atm_manuel.dat'
+        # sbm.scriviinputmanuel(alt_manuel,temp,pres,filename)
+        #
+        # mol_vmrs['CO2'] = CO2con
+        # mol_vmrs['O'] = Ocon
+        # filename = cart+'vmr_atm_manuel.dat'
+        # sbm.write_input_vmr_man(filename, alt_manuel, mol_vmrs, hit_gas_list = molist, hit_gas_num = molnums, version = 2)
+        #
+        # call(cart+'./fomi_mipas')
+        # nomeout = cart+'output__mipas.dat'
+        # alt_fomi, cr_fomi = sbm.leggioutfomi(nomeout)
+        alt_fomi, x_fomi, cr_fomi = npl.old_param(alts, temp, pres, CO2con, Oprof = Ocon, O2prof = O2con, N2prof = N2con, input_in_ppm = True)
 
-    #######
-    # spl = spline(alt_manuel, o2vmr)
-    # o2vmr_or = spl(alts_O)
-    # spl = spline(alt_manuel, n2vmr)
-    # n2vmr_or = spl(alts_O)
+        #######
+        # spl = spline(alt_manuel, o2vmr)
+        # o2vmr_or = spl(alts_O)
+        # spl = spline(alt_manuel, n2vmr)
+        # n2vmr_or = spl(alts_O)
 
-    #cr_new, debug = npl.new_param_full_allgrids(temp_or, temp_or[0], pres_or, CO2con_or*1.e-6, Ocon_or*1.e-6, o2vmr_or, n2vmr_or, interp_coeffs = interp_coeffs, debug = True)
-    cr_new, debug = npl.new_param_full_allgrids(temp, temp[0], pres, co2vmr, ovmr, o2vmr, n2vmr, interp_coeffs = interp_coeffs, debug = True)
+        #cr_new, debug = npl.new_param_full_allgrids(temp_or, temp_or[0], pres_or, CO2con_or*1.e-6, Ocon_or*1.e-6, o2vmr_or, n2vmr_or, interp_coeffs = interp_coeffs, debug = True)
+        cr_new, debug = npl.new_param_full_allgrids(temp, temp[0], pres, co2vmr, ovmr, o2vmr, n2vmr, interp_coeffs = interp_coeffs, debug = True)
 
-    debug_alphafit.append(debug['alpha_fit'])
-    alpha_debug.append(debug['alpha'])
-    L_esc_debug.append(debug['L_esc'])
-    co2column_debug.append(debug['co2_column'])
+        debug_alphafit.append(debug['alpha_fit'])
+        alpha_debug.append(debug['alpha'])
+        L_esc_debug.append(debug['L_esc'])
+        co2column_debug.append(debug['co2_column'])
 
-    alpha_fom = np.array([1.68717503, 1.52970568, 1.36024627, 1.18849647, 1.0773977, 1.02616183])
-    fomialpha = np.append(alpha_fom, np.ones(9))
+        alpha_fom = np.array([1.68717503, 1.52970568, 1.36024627, 1.18849647, 1.0773977, 1.02616183])
+        fomialpha = np.append(alpha_fom, np.ones(9))
 
-    cr_new_fa = npl.new_param_full_allgrids(temp, temp[0], pres, co2vmr, ovmr, o2vmr, n2vmr, interp_coeffs = interp_coeffs, debug = False, debug_alpha = fomialpha)
+        cr_new_fa = npl.new_param_full_allgrids(temp, temp[0], pres, co2vmr, ovmr, o2vmr, n2vmr, interp_coeffs = interp_coeffs, debug = False, debug_alpha = fomialpha)
 
-    co2vmr_ref = coeffs['co2profs'][2]
-    cr_new_fixco2 = npl.new_param_full_allgrids(temp, temp[0], pres, co2vmr, ovmr, o2vmr, n2vmr, interp_coeffs = interp_coeffs, debug = False, debug_co2interp = co2vmr_ref)
+        co2vmr_ref = coeffs['co2profs'][2]
+        cr_new_fixco2 = npl.new_param_full_allgrids(temp, temp[0], pres, co2vmr, ovmr, o2vmr, n2vmr, interp_coeffs = interp_coeffs, debug = False, debug_co2interp = co2vmr_ref)
 
-    ####
-    new_param.append(cr_new)
-    new_param_fa.append(cr_new_fa)
-    new_param_fixco2.append(cr_new_fixco2)
+        ####
+        new_param.append(cr_new)
+        new_param_fa.append(cr_new_fa)
+        new_param_fixco2.append(cr_new_fixco2)
 
-    res.date[0] = CR.date[il]
-    res.latitude[0] = CR.latitude[il]
-    res.longitude[0] = CR.longitude[il]
-    res.sza[0] = CR.sza[il]
-    # res.altitude[0] = CR.altitude[il]
-    # res.pressure[0] = CR.pressure[il]
-    # res.temperature[0] = CR.temperature[il]
-    res.altitude[0] = alts
-    res.pressure[0] = pres
-    res.temperature[0] = temp
+        res.date[0] = CR.date[il]
+        res.latitude[0] = CR.latitude[il]
+        res.longitude[0] = CR.longitude[il]
+        res.sza[0] = CR.sza[il]
+        # res.altitude[0] = CR.altitude[il]
+        # res.pressure[0] = CR.pressure[il]
+        # res.temperature[0] = CR.temperature[il]
+        res.altitude[0] = alts
+        res.pressure[0] = pres
+        res.temperature[0] = temp
 
-    res.cr_mipas[0] = CR.target[il]
-    obs.append(CR.target[il])
-    res.alt_fomi[0] = alt_fomi
-    res.cr_fomi[0] = cr_fomi
-    res.cr_new[0] = cr_new
+        res.cr_mipas[0] = CR.target[il]
+        obs.append(CR.target[il])
+        res.alt_fomi[0] = alt_fomi
+        res.cr_fomi[0] = cr_fomi
+        res.cr_new[0] = cr_new
 
-    splcr = spline(alt_fomi, cr_fomi)
-    cr_fom_ok = splcr(res.altitude[0])
-    old_param.append(cr_fom_ok)
-    res.cr_fomi_int[0] = cr_fom_ok
+        splcr = spline(alt_fomi, cr_fomi)
+        cr_fom_ok = splcr(res.altitude[0])
+        old_param.append(cr_fom_ok)
+        res.cr_fomi_int[0] = cr_fom_ok
 
-    restot = np.append(restot,res)
+        restot = np.append(restot,res)
 
 
-for nam in ['temp', 'pres', 'ovmr', 'co2vmr', 'o2vmr', 'n2vmr', 'cr_mipas', 'x']:
-    inputs[nam] = np.array(inputs[nam])
+    for nam in ['temp', 'pres', 'ovmr', 'co2vmr', 'o2vmr', 'n2vmr', 'cr_mipas', 'x']:
+        inputs[nam] = np.array(inputs[nam])
 
-restot = restot[1:]
-restot = restot.view(np.recarray)
+    restot = restot[1:]
+    restot = restot.view(np.recarray)
 
-pickle.dump(restot, open(cart_out+'ssw2009_{}.p'.format(ctag),'wb'))
+    pickle.dump(restot, open(cart_out+'ssw2009_{}.p'.format(ctag),'wb'))
+    pickle.dump([obs, old_param, new_param, new_param_fa, new_param_fixco2], open(cart_out+'out_ssw2009_{}.p'.format(ctag),'wb'))
+    pickle.dump(inputs, open(cart_out+'in_ssw2009_{}.p'.format(ctag),'wb'))
+    pickle.dump([alpha_debug, L_esc_debug, co2column_debug, debug_alphafit], open(cart_out+'debug_ssw2009_{}.p'.format(ctag),'wb'))
+else:
+    restot = pickle.load(open(cart_out+'ssw2009_{}.p'.format(ctag),'rb'))
+    obs, old_param, new_param, new_param_fa, new_param_fixco2 = pickle.load(open(cart_out+'out_ssw2009_{}.p'.format(ctag),'rb'))
+    inputs = pickle.load(open(cart_out+'in_ssw2009_{}.p'.format(ctag),'rb'))
+    alpha_debug, L_esc_debug, co2column_debug, debug_alphafit = pickle.load(open(cart_out+'debug_ssw2009_{}.p'.format(ctag),'rb'))
 
-pickle.dump([obs, old_param, new_param, new_param_fa, new_param_fixco2], open(cart_out+'out_ssw2009_{}.p'.format(ctag),'wb'))
+old_param = np.stack(old_param)
+obs = np.stack(obs)
+new_param = np.stack(new_param)
+new_param_fa = np.stack(new_param_fa)
+new_param_fixco2 = np.stack(new_param_fixco2)
 
-pickle.dump(inputs, open(cart_out+'in_ssw2009_{}.p'.format(ctag),'wb'))
+# # produco atmosfera di input in formato manuel ->
+# # lancio fomi
+# # leggo output e salvo in una struttura
+# d_fom = []
+# d_new = []
+# d_new_fa = []
+#
+# for x, crmi, crnew, crnew_fa, crfom in zip(mipx, restot.cr_mipas, new_param, new_param_fa, old_param):
+#     spl = spline(x, crmi)
+#     crmi2 = spl(x_ref)
+#     spl = spline(x, crnew)
+#     crnew2 = spl(x_ref)
+#     spl = spline(x, crnew_fa)
+#     crnew_fa2 = spl(x_ref)
+#     spl = spline(x, crfom)
+#     crfom2 = spl(x_ref)
+#
+#     d_fom.append(crmi2 + crfom2)
+#     d_new.append(crnew2 + crmi2)
+#     d_new_fa.append(crnew_fa2 + crmi2)
+#
+# d_fom = np.stack(d_fom)
+# d_new = np.stack(d_new)
+# d_new_fa = np.stack(d_new_fa)
 
-pickle.dump([alpha_debug, L_esc_debug, co2column_debug, debug_alphafit], open(cart_out+'debug_ssw2009_{}.p'.format(ctag),'wb'))
+d_fom = old_param + obs
+d_new = new_param + obs
+d_new_fa = new_param_fa + obs
+d_new_fixco2 = new_param_fixco2 + obs
 
-# produco atmosfera di input in formato manuel ->
-# lancio fomi
-# leggo output e salvo in una struttura
-d_fom = []
-d_new = []
-d_new_fa = []
-
-for x, crmi, crnew, crnew_fa, crfom in zip(mipx, restot.cr_mipas, new_param, new_param_fa, old_param):
-    spl = spline(x, crmi)
-    crmi2 = spl(x_ref)
-    spl = spline(x, crnew)
-    crnew2 = spl(x_ref)
-    spl = spline(x, crnew_fa)
-    crnew_fa2 = spl(x_ref)
-    spl = spline(x, crfom)
-    crfom2 = spl(x_ref)
-
-    d_fom.append(crmi2 + crfom2)
-    d_new.append(crnew2 + crmi2)
-    d_new_fa.append(crnew_fa2 + crmi2)
-
-d_fom = np.stack(d_fom)
-d_new = np.stack(d_new)
-d_new_fa = np.stack(d_new_fa)
-
+d_stats = dict()
 ### Figure shading
 fig, ax = plt.subplots()
 
-dfo_median = np.median(d_fom, axis = 0)
-dfo_1st = np.percentile(d_fom, 25, axis = 0)
-dfo_3rd = np.percentile(d_fom, 75, axis = 0)
-dfo_std = np.std(d_fom, axis = 0)
+for co, na, col in zip([d_fom, d_new, d_new_fa, d_new_fixco2], ['fomi', 'new', 'new_fa', 'new_fixco2'], ['blue', 'red', 'orange', 'forestgreen']):
+    d_stats[(na, 'median')] = np.median(co, axis = 0)
+    d_stats[(na, '1st')] = np.percentile(co, 25, axis = 0)
+    d_stats[(na, '3rd')] = np.percentile(co, 75, axis = 0)
+    d_stats[(na, 'std')] = np.std(co, axis = 0)
+    d_stats[(na, 'mean')] = np.mean(co, axis = 0)
 
-dnw_median = np.median(d_new, axis = 0)
-dnw_1st = np.percentile(d_new, 25, axis = 0)
-dnw_3rd = np.percentile(d_new, 75, axis = 0)
-dnw_std = np.std(d_new, axis = 0)
-
-dnw_median_fa = np.median(d_new_fa, axis = 0)
-dnw_1st_fa = np.percentile(d_new_fa, 25, axis = 0)
-dnw_3rd_fa = np.percentile(d_new_fa, 75, axis = 0)
-dnw_std_fa = np.std(d_new_fa, axis = 0)
-
-# ax.fill_betweenx(x_ref, dfo_mean-dfo_std, dfo_mean+dfo_std, color = 'blue', alpha = 0.4)
-# ax.fill_betweenx(x_ref, dnw_mean-dnw_std, dnw_mean+dnw_std, color = 'red', alpha = 0.4)
-# ax.plot(dfo_mean, x_ref, color = 'blue', lw = 2)
-# ax.plot(dnw_mean, x_ref, color = 'red', lw = 2)
-
-ax.fill_betweenx(x_ref, dfo_1st, dfo_3rd, color = 'blue', alpha = 0.4)
-ax.fill_betweenx(x_ref, dnw_1st, dnw_3rd, color = 'red', alpha = 0.4)
-ax.plot(dfo_median, x_ref, color = 'blue', lw = 2)
-ax.plot(dnw_median, x_ref, color = 'red', lw = 2)
-ax.fill_betweenx(x_ref, dnw_1st_fa, dnw_3rd_fa, color = 'orange', alpha = 0.4)
-ax.plot(dnw_median_fa, x_ref, color = 'orange', lw = 2)
+    ax.fill_betweenx(x_ref, d_stats[(na, '1st')], d_stats[(na, '3rd')], color = col, alpha = 0.4)
+    ax.plot(d_stats[(na, 'median')], x_ref, color = col, lw = 2)
 
 ax.grid()
 ax.set_xlim(-10., 15.)
